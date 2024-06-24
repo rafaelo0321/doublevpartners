7d08ecce49d054383ae1111da1c52bb28c1921a1
rafaelo0321 <rafaninho21@gmail.com>
2024-06-23 21:39:31 -0500

    Inicio del proyecto

---
+++ src/main/java/com/pruebatecnica/doublevpartners/DoublevpartnersApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.doublevpartners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoublevpartnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoublevpartnersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebatecnica/doublevpartners/DoublevpartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.doublevpartners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoublevpartnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
